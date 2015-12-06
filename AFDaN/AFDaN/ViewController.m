3d09fb3050f259bac8b1394c50fbe84cb316be6b
apple <apple@xiechen-Mac.local>
2015-12-06 20:17:59 +0800

    初始化项目

---
+++ AFDaN/AFDaN/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  AFDaN
+//
+//  Created by apple on 15/12/6.
+//  Copyright © 2015年 apple. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    NSLog(@"AFDaN");
+
+
+
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
